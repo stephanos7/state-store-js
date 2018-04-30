bd62e5ee5005486c5c7011b755e535088a76ec4b
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-04-30 16:17:51 +0100

    add listeners array and subscribe/unsubscribe property

--- store.js
+++ store.js
@@ -12,16 +12,35 @@ function Store(){
   let state = {
 
   }
+  // 4a.provide a way to listen for changes on the state
+  //    the user will want to subscribe to the store to listen to changes 
+  //    so we need to collect every fn passed in whenever the user subscribes to the store
+  //    we can define an array to track these
+  let listeners = [];
 
   // interfaces
   // 3. provide a way to get access to the state variable
   const getState  = () => state
 
-  // 4. provide a way to listen for changes on the state
+  // 4b.provide a way to listen for changes on the state
+  //    provide a subscribe method for the user to subscribe to the store and listen for changes
+  const subscribe = (listenerFnPassed) => {
+    // push the listener fn provided by the user to the listeners array defined in the prev step
+    listeners.push(listenerFnPassed)
+    // we should also provide the user with a way to unsubscribe so we can return a fn that does just that
+    // the function filters out  from the listeners array the function that was originally passed on subscribe
+    return () => {
+      listeners.filter(l => { 
+         l !== listenerFnPassed;
+      })
+    }
+  }   
+
 
 
   // return an object to the user containing the stores' interface properties 
   return {
-    getState
+    getState,
+    subscribe
   }
 }
